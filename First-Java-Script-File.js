196a101ed47d9e1353a9fb2eec6d30aaa2245c48
Xwill <89924712+xwillxu@users.noreply.github.com>
2022-02-26 11:36:24 -0500

    Update First-Java-Script-File.js

--- First-Java-Script-File.js
+++ First-Java-Script-File.js
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>Hello World</h1>
-<p>My First Java-Script-File</p>
-
-<p id="demo"></p>
-
-<script>
-document.getElementById("demo").innerHTML = 5 + 6;
-</script>
-
-</body>
-</html>
+console.log("Hello World")
